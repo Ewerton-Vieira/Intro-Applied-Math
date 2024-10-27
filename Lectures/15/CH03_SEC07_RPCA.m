589df1305d4b389d75520a171c2587720e6d3956
Ewerton-Vieira <ewertonrvieira@gmail.com>
2024-10-27 15:28:52 -0400

    lecture 15

---
+++ Lectures/15/CH03_SEC07_RPCA.m
@@ -0,0 +1,22 @@
+clear all, close all, clc
+
+load ../DATA/allFaces.mat
+X = faces(:,1:nfaces(1));
+[L,S] = RPCA(X);
+
+
+%%
+close all
+inds = [3 4 14 15 17 18 19 20 21 32 43];
+inds = [3 4];
+for k=inds
+    k
+    figure
+    subplot(2,2,1)
+    imagesc(reshape(X(:,k),192,168)), colormap gray
+    subplot(2,2,3)
+    imagesc(reshape(L(:,k),192,168)), colormap gray
+    subplot(2,2,4)
+    imagesc(reshape(S(:,k),192,168)), colormap gray
+    % pause
+end
